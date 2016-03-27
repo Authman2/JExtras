f464aeef79461a7958f3200e7a2a70d38a9aa8d0
Authman2 <authman2@gmail.com>
2016-03-26 22:31:10 -0400

    Update Save.java

--- JExtras/src/contents/Save.java
+++ JExtras/src/contents/Save.java
@@ -4,7 +4,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-//Not an actually drawn state, just used for saving data.
 public class Save {
 
 	public Save() {}
