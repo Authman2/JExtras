7d03420e3603a6a043ea6546d1daf4dac2b19cea
Adeola Uthman <authman2@gmail.com>
2017-01-28 14:34:05 -0500

    Updated TransitionManager class

--- JExtras/src/je/visual/TransitionManager.java
+++ JExtras/src/je/visual/TransitionManager.java
@@ -27,67 +27,238 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 @version 1.0
 */
 public class TransitionManager {
-
-	private Transition transition;
-	private boolean startFading;
-	private float alpha;
-	private double rateOfChange;
 	
-	public TransitionManager() {}
+	///////////////////////
+	//
+	//	Variables
+	//
+	///////////////////////
+	
+	private Transition transition;
+	private boolean started;
+	private float alpha;
+	private float rateOfChange;
+	private int width, height, startx, starty, endx, endy;
+	private Completion complete;
+	
+	
+	///////////////////////
+	//
+	//	Constructor
+	//
+	///////////////////////
+	
+	public TransitionManager(int w, int h) { width = w; height = h; }
+	
+	
+	
+	
+	///////////////////////
+	//
+	//	Setters
+	//
+	///////////////////////
 	
 	/** Sets the type of transition to perform as well as the amount to transition by. 
 	 * @param t -- The type of transition to perform.
 	 * @param rateOfChange -- The amount by which to change the drawings on the screen.
 	 */
-	public void SetTransition(Transition t, double rateOfChange) { this.transition = t; this.rateOfChange = rateOfChange; initialize(); }
+	public void setTransition(Transition t, float rateOfChange) { this.transition = t; this.rateOfChange = rateOfChange; initialize(); }
 	
-	/** Returns the current type of transition that this TransitionManager is set to perform. 
-	 * @return transition -- the transition that is currently set to be performed. */
-	public Transition getTransition() { return transition; }
 	
-	/** Boolean for whether or not this TransitionManager's transition type has been set. Mostly for error checking purposes.
-	 * @return returns whether or not the transition has already been set. */
-	public boolean transitionSet() { return transition != null; }
-	
-	/** Tells the TransitionManager to start performing the transition.. */
-	public void BeginTransition() { startFading = true; }
-	
-	/** Returns whether or not the TransitionManager is done with the current transition. 
-	 * @return returns whether or not the transition has finished playing. */
-	public boolean Finished() { 
-		if(transition == Transition.Fade_In) return alpha <= 0;
-		else return alpha >= 1;
+	/** Initializes certain variables after the the transition has been set. */
+	private void initialize() {
+		started = false;
+		
+		switch(transition) {
+			case Fade_In:
+				alpha = 1; // Starts dark, becomes lighter over time.
+				break;
+			case Fade_Out:
+				alpha = 0; // Starts light, becomes darker over time.
+				break;
+			case ScreenWipe_Up:
+				alpha = 1; // For all swipes, it remains dark the whole way through.
+				startx = 0;
+				endx = width;
+				starty = height;
+				endy = height;
+				break;
+			case ScreenWipe_Down:
+				alpha = 1;
+				startx = 0;
+				endx = width;
+				starty = -height;
+				endy = height;
+				break;
+			case ScreenWipe_Left:
+				alpha = 1;
+				startx = -width;
+				endx = width;
+				starty = 0;
+				endy = height;
+				break;
+			case ScreenWipe_Right:
+				alpha = 1;
+				startx = width;
+				endx = width*2;
+				starty = 0;
+				endy = height;
+				break;
+			default:
+				alpha = 1;
+				break;
+		}
+		
 	}
 	
 	
-	
-	public void initialize() {
-		if(transition == Transition.Fade_In) { alpha = 1; }
-		if(transition == Transition.Fade_Out) { alpha = 0; }
+	/** Starts the transition. 
+	 * @param completion -- An interface containing a method that gets called when the transition is finished. */
+	public void beginTransition(Completion completion) {
+		started = true;
+		complete = completion;
 	}
 	
-	public void update(double time) {
-		if(startFading && Finished() == false) {
-			//FADING IN
-			if(transition == Transition.Fade_In) {
-				//Lower the alpha value.
-				alpha -= rateOfChange;
-			}
-			//FADING OUT
-			if(transition == Transition.Fade_Out) {
-				//Raise the alpha value.
-				alpha += rateOfChange; 
+	
+	/** Updates all of the transitions. */
+	public void updateTransitions() {
+		if( started == true ) {
+			if(isFinished()) { if(complete != null) {complete.completion();} started = false; }
+			
+			switch(transition) {
+				case Fade_In:
+					fadeIn();
+					break;
+				case Fade_Out:
+					fadeOut();
+					break;
+				case ScreenWipe_Up:
+					swipeUp();
+					break;
+				case ScreenWipe_Down:
+					swipeDown();
+					break;
+				case ScreenWipe_Left:
+					swipeLeft();
+					break;
+				case ScreenWipe_Right:
+					swipeRight();
+					break;
+				default:
+					break;
 			}
 		}
 	}
 	
-	public void draw(Graphics2D g) {
-		if(startFading && Finished() == false) {
-			g.setColor(new Color(0,0,0,alpha));
-			g.fillRect(0, 0, 640, 470);
+	
+	/** Draw the transitions using a graphics object. */
+	public void drawTransitions(Graphics2D g) {
+		if( started == true ) {
+			
+			switch(transition) {
+				case Fade_In:
+					g.setColor(new Color(0,0,0,alpha));
+					g.fillRect(0, 0, width, height);
+					break;
+				case Fade_Out:
+					g.setColor(new Color(0,0,0,alpha));
+					g.fillRect(0, 0, width, height);
+					break;
+				case ScreenWipe_Up:
+					g.setColor(new Color(0,0,0,alpha));
+					g.fillRect(startx, starty, endx, endy);
+					break;
+				case ScreenWipe_Down:
+					g.setColor(new Color(0,0,0,alpha));
+					g.fillRect(startx, starty, endx, endy);
+					break;
+				case ScreenWipe_Left:
+					g.setColor(new Color(0,0,0,alpha));
+					g.fillRect(startx, starty, endx, endy);
+					break;
+				case ScreenWipe_Right:
+					g.setColor(new Color(0,0,0,alpha));
+					g.fillRect(startx, starty, endx, endy);
+					break;
+				default:
+					break;
+			}
 		}
 	}
-
+	
+	
+	
+	/** Performs the fade in animation. */
+	private void fadeIn() {
+		if(alpha > 0) {
+			alpha -= rateOfChange;
+			if(alpha < 0) { alpha = 0; }
+		}
+	}
+	
+	/** Performs the fade out animation. */
+	private void fadeOut() {
+		if(alpha < 1) {
+			alpha += rateOfChange;
+			if(alpha > 1) { alpha = 1; }
+		}
+	}
+	
+	/** Performs the swipe up animation. */
+	private void swipeUp() {
+		starty -= rateOfChange;
+	}
+	
+	/** Performs the swipe down animation. */
+	private void swipeDown() {
+		starty += rateOfChange;
+	}
+	
+	/** Performs the swipe left animation. */
+	private void swipeLeft() {
+		startx += rateOfChange;
+	}
+	
+	/** Performs the swipe right animation. */
+	private void swipeRight() {
+		startx -= rateOfChange;
+	}
+	
+	
+	
+	public boolean isFinished() {
+		boolean finished = false;
+		if( started == true ) {
+			
+			switch(transition) {
+				case Fade_In:
+					if(alpha <= 0) finished = true;
+					break;
+				case Fade_Out:
+					if(alpha >= 1) finished = true;
+					break;
+				case ScreenWipe_Up:
+					if(starty <= 0) finished = true;
+					break;
+				case ScreenWipe_Down:
+					if(starty >= 0) finished = true;
+					break;
+				case ScreenWipe_Left:
+					if(startx >= 0) finished = true;
+					break;
+				case ScreenWipe_Right:
+					if(startx <= 0) finished = true;
+					break;
+				default:
+					finished = true;
+					break;
+			}
+		}
+		return finished;
+	}
+	
+	
 	
 	
 	
@@ -98,4 +269,8 @@ public class TransitionManager {
 		Fade_Out, Fade_In, ScreenWipe_Up, ScreenWipe_Down, ScreenWipe_Left, ScreenWipe_Right;
 	}
 
+	/** A completion block interface. */
+	public interface Completion {
+		public void completion();
+	}
 }
